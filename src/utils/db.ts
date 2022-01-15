aae19b3d116f3c540c4dc65f9fef9f76d7c834e2
Jeffcw96 <changcheewah96@gmail.com>
2022-01-15 21:10:33 +0800

    add docker file

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -2,11 +2,12 @@ import mongoose from 'mongoose'
 
 
 async function connect() {
-    const dbUri = process.env.mongoConnection || 'mongodb://localhost:27017/todo'
+    const dbUri = process.env.mongoConnection || 'mongodb://mongo:27017/todo'
     try {
         await mongoose.connect(dbUri)
         console.log("Connected to DB")
     } catch (error) {
+        console.log("ERROR",error)
         console.error("Failed Connect to DB")
         process.exit(1)
     }
